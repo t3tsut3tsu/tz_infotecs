ab6f5c07d4ef25233504b4ce8a3170f667d22ba6
t3tsut3tsu <thevalya9@mail.ru>
2025-02-20 22:47:57 +0700

    add minimax, avg stats

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import requests
+import time
 
 def arg_parser():
     parser = argparse.ArgumentParser(description="Simple HTTP servers tester")
@@ -12,11 +13,17 @@ def response_getting(args):
     for host in args.hosts:
         cnt_suc = 0
         cnt_fail = 0
+        cnt_err = 0
+        pinging = []
+        print('------------------------------------')
         print(f'Host: {host}')
         for _ in range(args.counts):
             try:
+                start_ping = time.time() #начало запроса
                 response = requests.get(host)
-                print(f'Time: {response.elapsed.total_seconds()}')
+                end_ping = time.time() #конец запроса
+                ping = end_ping - start_ping
+                pinging.append(ping)
                 if response.status_code == 200:
                     cnt_suc += 1
                 elif response.status_code == 400 or response.status_code == 500:
@@ -24,10 +31,17 @@ def response_getting(args):
                 #print(f'The answer is: {response.status_code} for {host}') #отладка
             except requests.exceptions.ConnectionError:
                 print(f'Dns lookup failed for {host}')
+                cnt_err += 1
             except requests.exceptions.ReadTimeout:
                 print(f'Read timeout for {host}')
+                cnt_err += 1
+        if pinging:
+            print(f'Min: {round(min(pinging), 3)}')
+            print(f'Max: {round(max(pinging), 3)}')
+            print(f'Avg: {round((sum(pinging)/args.counts), 3)}')
         print(f'Success: {cnt_suc}')
         print(f'Failed: {cnt_fail}')
+        print(f'Errors: {cnt_err}')
 
 args = arg_parser()
 response_getting(args)
